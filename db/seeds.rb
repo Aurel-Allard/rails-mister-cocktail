075e2289be6c6221317da8a569000dae74ed0a52
Aurelien Allard <allard.au@gmail.com>
2017-11-24 17:42:47 +0100

    Landing page designed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,36 @@
 require 'json'
 require 'open-uri'
 
+cocktail = Cocktail.new(name: "Old Fashioned")
+url =  "http://www.seriouseats.com/images/2014/11/20141104-cocktail-party-old-fashioneds-holiday-vicky-wasik-3.jpg"
+cocktail.remote_photo_url = url
+cocktail.save
+
+cocktail = Cocktail.new(name: "Daiquiri")
+url =  "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-daiquiri.jpg"
+cocktail.remote_photo_url = url
+cocktail.save
+
+cocktail = Cocktail.new(name: "Margarita")
+url =  "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-margarita.jpg"
+cocktail.remote_photo_url = url
+cocktail.save
+
+cocktail = Cocktail.new(name: "Mojito")
+url =  "http://www.seriouseats.com/recipes/assets_c/2011/10/20111019mojitoprim2-thumb-625xauto-194084.jpg"
+cocktail.remote_photo_url = url
+cocktail.save
+
+cocktail = Cocktail.new(name: "Cuba Libre")
+url =  "http://www.seriouseats.com/recipes/assets_c/2015/06/20150622-3-ingredient-cocktail-cuba-libre-vicky-wasik-thumb-1500xauto-424377.jpg"
+cocktail.remote_photo_url = url
+cocktail.save
+
+cocktail = Cocktail.new(name: "Moscow Mule")
+url =  "http://www.seriouseats.com/images/2017/06/20170607-summer-vodka-recipes-roundup-15.jpg"
+cocktail.remote_photo_url = url
+cocktail.save
+
 url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ing_data = open(url).read
 ing_list = JSON.parse(ing_data)
